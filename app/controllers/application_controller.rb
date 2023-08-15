df6b8971a7998c1dd12e3ad11ae01d5e1ba5c272
takushiz <takushi19850503@gmail.com>
2023-08-15 13:42:54 +0900

    購入ページ：送料負担表示、editアクション リダイレクト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :configure_permitted_parameter, if: :devise_controller?
 
   private
